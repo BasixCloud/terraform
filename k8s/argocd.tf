2fb35b74cf70c8da70dbcf6501a35c6e8f41fe2c
Basix <basix@basix.tech>
2024-05-01 16:27:08 +0900

    Move things out

--- k8s/argocd.tf
+++ k8s/argocd.tf
@@ -1,36 +1,21 @@
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
-
 variable "argocd_version" {
   description = "Argo CD version to deploy."
   type        = string
   default     = "v2.10.8"
 }
 
-data "http" "argocd_install_yaml" {
-  url = format("https://raw.githubusercontent.com/argoproj/argo-cd/%s/manifests/install.yaml", var.argocd_version)
-
-  # Optional request headers
-  request_headers = {
-    Accept = "application/x-yaml"
-  }
-}
-
-data "kubectl_file_documents" "argocd_install_documents" {
-  content = data.http.argocd_install_yaml.response_body
-}
-
-resource "kubectl_manifest" "argocd_install" {
-  for_each           = data.kubectl_file_documents.argocd_install_documents.manifests
-  yaml_body          = each.value
-  override_namespace = kubernetes_namespace.argocd.id
-}
-
 module "argocd" {
   source = "../modules/remote-manifest"
   url =  format("https://raw.githubusercontent.com/argoproj/argo-cd/%s/manifests/install.yaml", var.argocd_version)
   namespace = "argocd"
+}
+
+moved {
+  from = kubectl_manifest.argocd_install
+  to = module.argocd.kubectl_manifest.manifest
+}
+
+moved {
+  from = kubernetes_namespace.argocd
+  to = module.argocd.kubernetes_namespace.namespace[0]
 }
--- modules/remote-manifest/outputs.tf
+++ modules/remote-manifest/outputs.tf
@@ -2,10 +2,6 @@ output "namespace" {
   value = var.namespace != null ? kubernetes_namespace.namespace[0].id : null
 }
 
-output "uid" {
-  value = kubectl_manifest.manifest[*].uid
-}
-
-output "kind" {
-  value = kubectl_manifest.manifest[*].kind
+output "manifest" {
+  value = kubectl_manifest.manifest
 }
