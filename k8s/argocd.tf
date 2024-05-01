38e025fa302124599101756ee27f28bd0feca172
Basix <basix@basix.tech>
2024-05-01 16:39:58 +0900

    moved moved

--- k8s/argocd.tf
+++ k8s/argocd.tf
@@ -10,3 +10,13 @@ module "argocd" {
   namespace = "argocd"
 }
 
+moved {
+  from = kubectl_manifest.argocd_install
+  to = module.argocd.kubectl_manifest.manifest
+}
+
+moved {
+  from = kubernetes_namespace.argocd
+  to = module.argocd.kubernetes_namespace.namespace[0]
+}
+
--- k8s/migrations/20240501-argocd.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = kubectl_manifest.argocd_install
-  to = module.argocd.kubectl_manifest.manifest
-}
-
-moved {
-  from = kubernetes_namespace.argocd
-  to = module.argocd.kubernetes_namespace.namespace[0]
-}
